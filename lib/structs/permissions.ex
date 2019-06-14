ea63f3917afdb0f2c74afdd8fc26a45356920f66
Johannes Christ <jc@jchri.st>
2019-06-14 21:18:00 +0200

    Dedent codeblock to match other code.

--- lib/structs/permissions.ex
+++ lib/structs/permissions.ex
@@ -319,20 +319,20 @@ defmodule Crux.Structs.Permissions do
 
   ## Examples
     ```elixir
-    iex> Crux.Structs.Permissions.missing([:send_messages, :view_channel], [:send_messages, :view_channel, :embed_links])
-    %Crux.Structs.Permissions{bitfield: 0x4000}
+  iex> Crux.Structs.Permissions.missing([:send_messages, :view_channel], [:send_messages, :view_channel, :embed_links])
+  %Crux.Structs.Permissions{bitfield: 0x4000}
 
-    # Administrator won't implicilty grant other permissions
-    iex> Crux.Structs.Permissions.missing([:administrator], [:send_messages])
-    %Crux.Structs.Permissions{bitfield: 0x800}
+  # Administrator won't implicilty grant other permissions
+  iex> Crux.Structs.Permissions.missing([:administrator], [:send_messages])
+  %Crux.Structs.Permissions{bitfield: 0x800}
 
-    # Everything set
-    iex> Crux.Structs.Permissions.missing([:kick_members, :ban_members, :view_audit_log], [:kick_members, :ban_members])
-    %Crux.Structs.Permissions{bitfield: 0}
+  # Everything set
+  iex> Crux.Structs.Permissions.missing([:kick_members, :ban_members, :view_audit_log], [:kick_members, :ban_members])
+  %Crux.Structs.Permissions{bitfield: 0}
 
-    # No permissions
-    iex> Crux.Structs.Permissions.missing([:send_messages, :view_channel], [])
-    %Crux.Structs.Permissions{bitfield: 0}
+  # No permissions
+  iex> Crux.Structs.Permissions.missing([:send_messages, :view_channel], [])
+  %Crux.Structs.Permissions{bitfield: 0}
     ```
   """
   @spec missing(resolvable(), resolvable()) :: t()
