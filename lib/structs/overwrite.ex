3c281f3c80ca707a310c855d6bc190a0b6d4ab6d
SpaceEEC <spaceeec@yahoo.com>
2019-09-04 17:38:00 +0200

    feat(Overwrite): add target_resolvable

--- lib/structs/overwrite.ex
+++ lib/structs/overwrite.ex
@@ -5,8 +5,9 @@ defmodule Crux.Structs.Overwrite do
 
   @behaviour Crux.Structs
 
-  alias Crux.Structs.{Snowflake, Util}
+  alias Crux.Structs.{Overwrite, Role, Snowflake, User, Util}
   require Util
+  require Snowflake
 
   Util.modulesince("0.1.0")
 
@@ -25,6 +26,70 @@ defmodule Crux.Structs.Overwrite do
           allow: integer(),
           deny: integer()
         }
+
+  @typedoc """
+    All available types that can be resolved into a target for a permission overwrite
+  """
+  Util.typesince("0.2.1")
+  @type target_resolvable() :: Overwrite.t() | Role.t() | User.id_resolvable()
+
+  @doc """
+    Resolves a `t:target_resolvable/0` into an overwrite target.
+
+  > Note that an id or string of it returns `:unknown` as type.
+
+  ## Examples
+
+    ```elixir
+    iex> %Crux.Structs.Overwrite{type: "member", id: 218348062828003328}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"member", 218348062828003328}
+
+    iex> %Crux.Structs.Role{id: 376146940762783746}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"role", 376146940762783746}
+
+    iex> %Crux.Structs.User{id: 218348062828003328}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"member", 218348062828003328}
+
+    iex> %Crux.Structs.Member{user: 218348062828003328}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"member", 218348062828003328}
+
+    iex> %Crux.Structs.Message{author: %Crux.Structs.User{id: 218348062828003328}}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"member", 218348062828003328}
+
+    iex> %Crux.Structs.VoiceState{user_id: 218348062828003328}
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {"member", 218348062828003328}
+
+    iex> 218348062828003328
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {:unknown, 218348062828003328}
+
+    iex> "218348062828003328"
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    {:unknown, 218348062828003328}
+
+    iex> nil
+    ...> |> Crux.Structs.Overwrite.resolve_target()
+    nil
+
+    ```
+  """
+  @spec resolve_target(target_resolvable()) :: {String.t() | :unknown, Snowflake.t()}
+  def resolve_target(%Overwrite{id: id, type: type}), do: {type, id}
+  def resolve_target(%Role{id: id}), do: {"role", id}
+
+  def resolve_target(target) do
+    case Crux.Structs.resolve_id(target, User) do
+      nil -> nil
+      id when is_map(target) -> {"member", id}
+      id -> {:unknown, id}
+    end
+  end
 
   @doc """
     Creates a `t:Crux.Structs.Overwrite.t/0` struct from raw data.
