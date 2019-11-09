65020926e8bdf9b94d5cd22cc46a3de840f18733
SpaceEEC <spaceeec@yahoo.com>
2019-11-09 20:00:57 +0100

    docs(Invite): add missing arity

--- lib/structs/invite.ex
+++ lib/structs/invite.ex
@@ -4,21 +4,21 @@ defmodule Crux.Structs.Invite do
 
   List of what property can be present fetched with what function:
 
-  | Property                   | `Rest.get_guild_vanity_invite` | `Rest.get_invite/1` | `Rest.create_channel_invite/1` | `Rest.delete_invite/1`   |
-  |                            |                                |                     | `Rest.get_channel_invites/1`   |                          |
-  |                            |                                |                     | `Rest.get_guild_invites/1`     |                          |
-  | code                       | yes                            | yes                 | yes                            | yes                      |
-  | guild                      | no                             | if not group dm     | if not group dm                | if not group dm          |
-  | channel                    | no                             | yes                 | yes                            | yes                      |
-  | inviter                    | no                             | yes                 | yes                            | yes                      |
-  | uses                       | yes                            | no                  | yes                            | no                       |
-  | max_uses                   | no                             | no                  | yes                            | no                       |
-  | max_age                    | no                             | no                  | yes                            | no                       |
-  | temporary                  | no                             | no                  | yes                            | no                       |
-  | created_at                 | no                             | no                  | yes                            | no                       |
-  | revoked                    | no                             | no                  | no                             | no                       |
-  | approximate_presence_count | no                             | yes                 | no                             | no                       |
-  | approximate_member_count   | no                             | yes                 | no                             | no                       |
+  | Property                   | `Rest.get_guild_vanity_invite/1` | `Rest.get_invite/1` | `Rest.create_channel_invite/1` | `Rest.delete_invite/1`   |
+  |                            |                                  |                     | `Rest.get_channel_invites/1`   |                          |
+  |                            |                                  |                     | `Rest.get_guild_invites/1`     |                          |
+  | code                       | yes                              | yes                 | yes                            | yes                      |
+  | guild                      | no                               | if not group dm     | if not group dm                | if not group dm          |
+  | channel                    | no                               | yes                 | yes                            | yes                      |
+  | inviter                    | no                               | yes                 | yes                            | yes                      |
+  | uses                       | yes                              | no                  | yes                            | no                       |
+  | max_uses                   | no                               | no                  | yes                            | no                       |
+  | max_age                    | no                               | no                  | yes                            | no                       |
+  | temporary                  | no                               | no                  | yes                            | no                       |
+  | created_at                 | no                               | no                  | yes                            | no                       |
+  | revoked                    | no                               | no                  | no                             | no                       |
+  | approximate_presence_count | no                               | yes                 | no                             | no                       |
+  | approximate_member_count   | no                               | yes                 | no                             | no                       |
 
   Notes:
   - `:guild` only has`:verification_level`, `:features`, `:name`, `:splash`, `:id`, and `:icon`.
