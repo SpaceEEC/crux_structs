51daca9e3eebeefb9e59780e7dbffff2de57a11f
SpaceEEC <spaceeec@yahoo.com>
2020-03-14 10:51:21 +0100

    feat(VoiceRegion): add voice region module and struct

---
+++ lib/structs/voice_region.ex
@@ -0,0 +1,32 @@
+defmodule Crux.Structs.VoiceRegion do
+  @moduledoc """
+    Represents a Discord [Voice Region Object](https://discordapp.com/developers/docs/resources/voice#voice-region-object).
+  """
+
+  @behaviour Crux.Structs
+
+  alias Crux.Structs.{Snowflake, Util, VoiceRegion}
+  require Util
+
+  Util.modulesince("0.2.3")
+
+  defstruct(
+    id: nil,
+    name: nil,
+    vip: nil,
+    optimal: nil,
+    deprecated: nil,
+    custom: nil
+  )
+
+  Util.typesince("0.2.3")
+
+  @type t :: %__MODULE__{
+          id: String.t(),
+          name: String.t(),
+          vip: boolean(),
+          optimal: boolean(),
+          deprecated: boolean(),
+          custom: boolean()
+        }
+end
