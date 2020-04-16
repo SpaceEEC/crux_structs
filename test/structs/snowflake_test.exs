ef454e12db389b5ba74bfc8046d8cb1a37bc96a7
SpaceEEC <spaceeec@yahoo.com>
2020-04-16 13:42:20 +0200

    test(Snowflake): test is_snowflake/1 guard

--- test/structs/snowflake_test.exs
+++ test/structs/snowflake_test.exs
@@ -23,7 +23,7 @@ defmodule Crux.Structs.SnowflakeTest do
   test "forth and back yields same result (start integer)" do
     id = 218_348_062_828_003_328
 
-    assert id === Snowflake.deconstruct(id) |> Snowflake.construct()
+    assert id === id |> Snowflake.deconstruct() |> Snowflake.construct()
   end
 
   test "forth and back yields same result (start struct)" do
@@ -34,6 +34,30 @@ defmodule Crux.Structs.SnowflakeTest do
       worker_id: 1
     }
 
-    assert snowflake === Snowflake.construct(snowflake) |> Snowflake.deconstruct()
+    assert snowflake === snowflake |> Snowflake.construct() |> Snowflake.deconstruct()
+  end
+
+  describe "is_snowflake/1 guard" do
+    require Snowflake
+
+    test "too small integer" do
+      refute Snowflake.is_snowflake(-1)
+    end
+
+    test "too large integer" do
+      refute Snowflake.is_snowflake(0x1_0000_0000_0000_0000)
+    end
+
+    test "float" do
+      refute Snowflake.is_snowflake(1.0)
+    end
+
+    test "string" do
+      refute Snowflake.is_snowflake("218348062828003328")
+    end
+
+    test "snowflake" do
+      assert Snowflake.is_snowflake(218_348_062_828_003_328)
+    end
   end
 end
