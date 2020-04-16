b75ebf046cc66eef10ad348ff35124778a1953b3
SpaceEEC <spaceeec@yahoo.com>
2020-04-16 15:51:30 +0200

    test(VoiceRegion): create/1

---
+++ test/structs/voice_region_test.exs
@@ -0,0 +1,29 @@
+defmodule Crux.Structs.VoiceRegionTest do
+  use ExUnit.Case, async: true
+  doctest Crux.Structs.VoiceRegion
+
+  alias Crux.Structs
+  alias Crux.Structs.VoiceRegion
+
+  test "create/1" do
+    voice_region =
+      %{
+        "id" => "us-west",
+        "name" => "US West",
+        "vip" => false,
+        "custom" => false,
+        "deprecated" => false,
+        "optimal" => false
+      }
+      |> Structs.create(VoiceState)
+
+    assert voice_region == %VoiceRegion{
+             id: "us-west",
+             name: "US West",
+             vip: false,
+             custom: false,
+             deprecated: false,
+             optimal: false
+           }
+  end
+end
